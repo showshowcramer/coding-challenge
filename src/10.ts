fa6c348692267247a2cf6505d4415c02470fd9da
showshowcramer <showshowcramer@gmail.com>
2025-03-16 13:40:59 +0000

    Add 10.ts

---
+++ src/10.ts
@@ -0,0 +1,6 @@
+// A simple TypeScript function that takes two numbers and returns their sum
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(sum(2, 3)); // Output: 5
