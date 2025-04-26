419a1eeb1de82d30b5c8f6d7ef4eb739f2725e79
showshowcramer <showshowcramer@gmail.com>
2025-04-26 09:22:42 +0000

    Add 33.ts

---
+++ src/33.ts
@@ -0,0 +1,14 @@
+function isPalindrome(str: string): boolean {
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
+}
